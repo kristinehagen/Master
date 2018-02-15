b90fd17782fd8459f49ae56b14bd82f4efd46b86
kristinehagen <hagenkristine@gmail.com>
2018-02-15 16:27:21 +0100

    Lager print til individ

--- src/classes/Individual.java
+++ src/classes/Individual.java
@@ -1,15 +1,43 @@
 package classes;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Individual {
 
     private double fitness;
     private Solution solution = new Solution();
 
+
+    //Constructor
     public Individual(Solution solution) {
         this.fitness = calculateFitness(solution);
         this.solution = solution;
     }
 
+    //Print
+    public void printIndividual(HashMap<Integer, Vehicle> vehicles) {
+        ArrayList<Integer> vehicleId = new ArrayList<>();
+        for (Vehicle vehicle: vehicles.values()) {
+            vehicleId.add(vehicle.getId());
+        }
+        for (int i = 0; i < this.solution.getSolution().size(); i++) {
+            System.out.print("Vehicle " + vehicleId.get(i) + ": ");
+            for (int j = 0; j < this.solution.getSolution().get(i).size(); j++) {
+                System.out.print(this.solution.getSolution().get(i).get(j).getId() + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+
+
+
+
+    //Getters and setters
 
     public void setFitness(double fitness) {
         this.fitness = fitness;
@@ -43,5 +71,6 @@ public class Individual {
     private double calculateFitness(Solution solution) {
         return 0;
     }
+
 
 }
